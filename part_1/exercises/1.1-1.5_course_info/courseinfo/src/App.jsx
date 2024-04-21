2f6d25fe8fbbb84b8e89d4ec5d76e589cd9ea0fc
Jerrybaoyilei <jerrybaoyilei@qq.com>
2024-04-21 19:29:11 -0400

    Exercise 1.4

--- part_1/exercises/1.1-1.5_course_info/courseinfo/src/App.jsx
+++ part_1/exercises/1.1-1.5_course_info/courseinfo/src/App.jsx
@@ -126,21 +126,41 @@ const App = () => {
 
   // 1.4
 
-  return (
-    <div>
+  const Header = ({ course }) => {
+    return (
       <h1>{course}</h1>
-      <p>
-        {parts[0].name} {parts[0].exercises}
-      </p>
-      <p>
-        {parts[1].name} {parts[1].exercises}
-      </p>
-      <p>
-        {parts[2].name} {parts[2].exercises}
-      </p>
+    )
+  }
+
+  const Content = ({ parts }) => {
+    return (
+      <div>
+        <p>
+          {parts[0].name} {parts[0].exercises}
+        </p>
+        <p>
+          {parts[1].name} {parts[1].exercises}
+        </p>
+        <p>
+          {parts[2].name} {parts[2].exercises}
+        </p>
+      </div>
+    )
+  }
+
+  const Total = ({ parts }) => {
+    return (
       <p>
         Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}
       </p>
+    )
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
