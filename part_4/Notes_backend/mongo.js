0fb5917747f981557833a7bc73a5cd39b83f8850
Yilei (Jerry) Bao <yileibao1998@gmail.com>
2024-06-04 17:15:39 -0400

    Part 5 progress update (including part 3 code)

--- part_4/Notes_backend/mongo.js
+++ part_4/Notes_backend/mongo.js
@@ -8,7 +8,7 @@ if (process.argv.length < 3) {
 const password = process.argv[2]
 
 const url =
-  `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/noteApp?retryWrites=true&w=majority`
+  `mongodb+srv://baoyil:${password}@cluster0.ykbloob.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url).then(() => {
@@ -19,9 +19,8 @@ mongoose.connect(url).then(() => {
 
   const Note = mongoose.model('Note', noteSchema)
 
-  /*
   const note = new Note({
-    content: 'HTML is x',
+    content: 'HTML is easy',
     important: true,
   })
 
@@ -29,7 +28,7 @@ mongoose.connect(url).then(() => {
     console.log('note saved!')
     mongoose.connection.close()
   })
-  */
+
   Note.find({}).then(result => {
     result.forEach(note => {
       console.log(note)
